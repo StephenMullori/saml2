6a66cc31d03a927713d45b3110cb27224c1e36fb
StephenMullori <smm200408@gmail.com>
2024-08-25 16:44:46 -0400

    this was useless

--- lib/saml2.ts
+++ lib/saml2.ts
@@ -50,24 +50,22 @@ function create_authn_request(
 
   const xml = create({
     AuthnRequest: {
-      "@xmlns": XMLNS.SAMLP,
-      "@xmlns:saml": XMLNS.SAML,
-      "@Version": "2.0",
-      "@ID": id,
-      "@IssueInstant": new Date().toISOString(),
-      "@Destination": destination,
-      "@AssertionConsumerServiceURL": assert_endpoint,
-      "@ProtocolBinding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
-      "@ForceAuthn": force_authn,
-      "saml:Issuer": issuer,
+      '@xmlns': XMLNS.SAMLP,
+      '@xmlns:saml': XMLNS.SAML,
+      '@Version': '2.0',
+      '@ID': id,
+      '@IssueInstant': (new Date()).toISOString(),
+      '@Destination': destination,
+      '@AssertionConsumerServiceURL': assert_endpoint,
+      '@ProtocolBinding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+      '@ForceAuthn': force_authn,
+      'saml:Issuer': issuer,
       NameIDPolicy: {
-        "@Format":
-          nameid_format ||
-          "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
-        "@AllowCreate": "true",
+        '@Format': nameid_format || 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+        '@AllowCreate': 'true'
       },
-      RequestedAuthnContext: context_element,
-    },
+      RequestedAuthnContext: context_element
+    }
   }).end();
 
   return { id, xml };
